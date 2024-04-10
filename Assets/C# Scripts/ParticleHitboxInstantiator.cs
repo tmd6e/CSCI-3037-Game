6183513dfdcb7d82a38b69f9fafae59af68786a2
csGiovanni <116302921+csGiovanni@users.noreply.github.com>
2024-04-09 20:51:56 -0500

    Fix Hitbox and Particle Spawning

--- Assets/C# Scripts/ParticleHitboxInstantiator.cs
+++ Assets/C# Scripts/ParticleHitboxInstantiator.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEditor.Progress;
 using static UnityEngine.ParticleSystem;
 
 public class ParticleHitboxInstantiator : MonoBehaviour
@@ -8,6 +11,7 @@ public class ParticleHitboxInstantiator : MonoBehaviour
     [SerializeField] public GameObject hitbox;
     private ParticleSystem attackParticleSystem;
     ParticleSystem.Particle[] particles;
+    List<GameObject> spawnedHitboxes = new List<GameObject>();
     int numParticlesAlive;
 
     private void Awake()
@@ -24,15 +28,20 @@ public class ParticleHitboxInstantiator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        numParticlesAlive = attackParticleSystem.GetParticles(particles);
-        Debug.Log(numParticlesAlive);
-        if (numParticlesAlive > 0)
-        {
-            for (int i = 0; i < numParticlesAlive; i++)
-            {
-                StartCoroutine(InstantHitbox(particles[i].position + attackParticleSystem.transform.position, particles[i].remainingLifetime));
-            }
-        }
+        //numParticlesAlive = attackParticleSystem.GetParticles(particles);
+        //Debug.Log(numParticlesAlive);
+        //if (numParticlesAlive > 0)
+        //{
+        //    for (int i = 0; i < numParticlesAlive; i++)
+        //    {
+        //        StartCoroutine(InstantHitbox(particles[i].position + attackParticleSystem.transform.position, particles[i].remainingLifetime));
+        //    }
+        //}
+
+        // Spawn / Destroy hitboxes to match live particles
+        RegulateParticleMapping();
+        // Once there are hitboxes, move them with the particles
+        MoveMappedParticles();
     }
 
     IEnumerator InstantHitbox(Vector3 particlePosition, float particleLifetime) {
@@ -42,5 +51,39 @@ public class ParticleHitboxInstantiator : MonoBehaviour
         Debug.Log("Destroying InstantHitbox");
         Destroy(instantHitbox);
         yield break;
+    }
+
+    void RegulateParticleMapping()
+    {
+        numParticlesAlive = attackParticleSystem.GetParticles(particles);
+        // If particle numbers do not match hitbox numbers, correct this
+        while (numParticlesAlive > spawnedHitboxes.Count)
+        {
+            spawnedHitboxes.Add(
+            Instantiate(hitbox, attackParticleSystem.transform.position, 
+            Quaternion.identity)
+            );
+        }
+        while (numParticlesAlive < spawnedHitboxes.Count)
+        {
+            Destroy(spawnedHitboxes[spawnedHitboxes.Count-1]);
+            spawnedHitboxes.RemoveAt(spawnedHitboxes.Count - 1);
+        }
+    }
+
+    void MoveMappedParticles()
+    {
+        numParticlesAlive = attackParticleSystem.GetParticles(particles);
+
+        // If there is an inbalance between hitboxes and particles, do nothing
+        if (numParticlesAlive != spawnedHitboxes.Count) return;
+
+        // Move hitboxes with particles
+        for (int i = 0; i < numParticlesAlive; i++)
+        {
+            ParticleSystem.Particle particle = particles[i];
+            spawnedHitboxes[i].transform.position = particle.position
+                     + attackParticleSystem.transform.position;
+        }
     }
 }
