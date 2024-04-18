c73fc6158e2807efc7271b7f6fcd5f2fb119bc8f
csGiovanni <116302921+csGiovanni@users.noreply.github.com>
2024-04-17 23:02:19 -0500

    Change Lighting and Adjust ForeverSpinState

--- Assets/C# Scripts/AI/StateScripts/ForeverSpinState.cs
+++ Assets/C# Scripts/AI/StateScripts/ForeverSpinState.cs
@@ -7,6 +7,10 @@ public class ForeverSpinState : AIState
 {
     public override AIState Tick(AICharacterManager aICharacter)
     {
+        if (aICharacter.isDead.Value)
+        {
+            return SwitchState(aICharacter, aICharacter.dead);
+        }
         // Simple AI to have an enemy spin in place. This is for a stationary mage.
         aICharacter.aiCharacterLocomotionManager.RotateStationary(aICharacter);
         return this;
